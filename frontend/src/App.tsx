50ea9fb9a96bbc30dc53d07863c478e1e4fe1a48
henriquesantosdev <henriquesantos2277@gmail.com>
2025-03-15 12:49:12 -0300

    chadcn configurated on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
+import { ErrorNotFound } from './pages/Error-not-found'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='*' element={<ErrorNotFound/>} />
     </Routes>
   )
 }
---
+++ frontend/src/pages/Error-not-found/index.tsx
@@ -0,0 +1,9 @@
+export const ErrorNotFound = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl">404</h1>
+      <h2 className="text-4xl">Page not found</h2>
+      
+    </div>
+  )
+}
